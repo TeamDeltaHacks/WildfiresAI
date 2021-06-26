ad4136b48dca1b0b92c23a8c25ac346e0ac5a95c
CMEONE <contact@kabirwearsato.ga>
2021-06-26 11:00:17 -0700

    routes

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/detect')
+def detect():
+    return render_template('detect.html')
 
-@app.route('/services')
-def services():
-    return render_template('services.html')
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
 
 @app.route('/visualize')
-def portfolio():
+def visualize():
     return render_template('visualize.html')
  
 if __name__ == "__main__":
