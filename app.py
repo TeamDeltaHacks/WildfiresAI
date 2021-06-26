be4ced679eb38a873c0828646fc492941d232e23
ShafinH <65793752+ShafinH@users.noreply.github.com>
2021-06-26 09:28:48 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
