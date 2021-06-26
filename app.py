c29e4698448606d3c5c4b9a34eb4bc2b5f8e58ae
CMEONE <contact@kabirwearsato.ga>
2021-06-26 13:11:11 -0700

    Server-side input validation

--- app.py
+++ app.py
@@ -26,16 +26,37 @@ def detect():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if(request.method == 'POST'):
-        latitude = request.form.get("latitude")
-        longitude = request.form.get("longitude")
-        month = request.form.get("month")
-        remoteness = request.form.get("remoteness")
-        temperature = request.form.get("temperature")
-        wind = request.form.get("wind")
-        humidity = request.form.get("humidity")
-        precipitation = request.form.get("precipitation")
-        vegetation = request.form.get("vegetation")
-        return render_template('predict.html')
+        output = "Unknown error"
+        try:
+            latitude = float(request.form.get("latitude"))
+            longitude = float(request.form.get("longitude"))
+            month = int(request.form.get("month"))
+            remoteness = float(request.form.get("remoteness"))
+            temperature = float(request.form.get("temperature"))
+            wind = float(request.form.get("wind"))
+            humidity = float(request.form.get("humidity"))
+            precipitation = float(request.form.get("precipitation"))
+            vegetation = int(request.form.get("vegetation"))
+            
+            assert (longitude >= -90)
+            assert (longitude <= 90)
+            assert (latitude >= -180)
+            assert (latitude <= 180)
+            assert (month >= 1)
+            assert (month <= 12)
+            assert (remoteness >= 0)
+            assert (temperature >= -274)
+            assert (wind >= 0)
+            assert (humidity >= 0)
+            assert (humidity <= 100)
+            assert (precipitation >= 0)
+            assert (vegetation >= 1)
+            assert (vegetation <= 28)
+            
+            output = "Output"
+        except:
+            output = "Invalid parameters"
+        return render_template('predict.html', output=output)
     else:
         return render_template('predict.html')
 
